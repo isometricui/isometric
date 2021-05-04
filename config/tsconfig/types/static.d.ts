225779c65371e26c2fc6874a01c6e97edf617201
Omer Balyali <omerbalyali@gmail.com>
2021-05-04 18:51:29 +0200

    [tsconfig] Add static file types

---
+++ config/tsconfig/types/static.d.ts
@@ -0,0 +1,37 @@
+/* Static file-type declarations */
+
+/* CSS */
+declare module '*.css';
+declare module '*.scss';
+
+/* CSS Modules */
+declare module '*.module.css' {
+  const classes: { [key: string]: string };
+  export default classes;
+}
+declare module '*.module.scss' {
+  const classes: { [key: string]: string };
+  export default classes;
+}
+
+/* Media */
+declare module '*.svg' {
+  const ref: string;
+  export default ref;
+}
+declare module '*.gif' {
+  const ref: string;
+  export default ref;
+}
+declare module '*.jpg' {
+  const ref: string;
+  export default ref;
+}
+declare module '*.jpeg' {
+  const ref: string;
+  export default ref;
+}
+declare module '*.png' {
+  const ref: string;
+  export default ref;
+}
