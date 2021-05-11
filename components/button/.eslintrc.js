db8c350a0f1525449cfdf75afc10739e99a91d0b
Omer Balyali <omerbalyali@gmail.com>
2021-05-11 17:09:25 +0200

    [button] Initial

---
+++ components/button/.eslintrc.js
@@ -0,0 +1,4 @@
+module.exports = {
+  extends: '@isometric/eslint-config/react',
+  parserOptions: { tsconfigRootDir: __dirname },
+};
---
+++ components/button/jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  coverageThreshold: {
+    global: {
+      statements: 0,
+      branches: 0,
+      functions: 0,
+      lines: 0,
+    },
+  },
+};
