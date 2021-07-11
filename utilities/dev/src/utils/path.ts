ad07e35ad06079559413c262d201520df9970f95
Omer Balyali <omerbalyali@gmail.com>
2021-07-11 19:03:51 +0200

    [dev-utils] Checks if the path exists under multiple parents

--- utilities/dev/src/utils/path.ts
+++ utilities/dev/src/utils/path.ts
@@ -2,28 +2,30 @@ import { paths } from '../common/common';
 import { logger } from './logger';
 
 async function checkParent({
-  parent,
+  parentPaths,
   path,
   execute,
   log,
 }: {
-  parent: string;
+  parentPaths: string[];
   path: string;
   execute: () => Promise<void> | void;
   log: { parent: string; task: string };
 }) {
-  if (path.includes(parent) && path !== parent) {
-    await execute();
-  } else {
-    logger(
-      `[${log.task}] This command can only be used in a subdirectory of ${log.parent} directory`
-    );
-  }
+  parentPaths.map(async (parent) => {
+    if (path.includes(parent) && path !== parent) {
+      await execute();
+    } else {
+      logger(
+        `[${log.task}] This command can only be used in a subdirectory of ${log.parent} directory`
+      );
+    }
+  });
 }
 
 async function checkCwdInComponents({ task }: { task: string }, execute: () => {}) {
   await checkParent({
-    parent: paths.components,
+    parentPaths: [paths.components, paths.utils],
     path: paths.root,
     log: { parent: 'components', task },
     execute: () => {
